d542f61a50235549742cd1dea6262e4f86ec0b29
unknown <zainabfarooq431@gmail.com>
2025-02-17 16:02:40 +0500

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 // import Image from "next/image";
 
+import Link from "next/link";
+import ProductCard from "./Components/ProductCard";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href='/user/new'>User</Link>
+      <ProductCard/>
     </main>
   );
 }
---
+++ app/user/new/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Newuser = () => {
+  return (
+    <div>
+      new user
+    </div>
+  )
+}
+
+export default Newuser
+
